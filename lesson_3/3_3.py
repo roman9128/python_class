cbefc1e7535a4e304a6610f790f7ba9c1d43b69d
roman <roma-t@bk.ru>
2024-03-09 22:30:45 +0300

    lesson_3

---
+++ lesson_3/3_3.py
@@ -0,0 +1,18 @@
+# Напишите программу для печати всех уникальных значений в словаре
+
+dictionary = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"}, {"V": "S009"}, {"VIII": "S007"}]
+s = []
+i = 0
+while i < len(dictionary):
+    for k, v in dictionary[i].items():
+        x = [k, v]
+    s = s + x
+    i += 1
+values_list = []
+j = 0
+while j < len(s):
+    values_list.append(s[j+1])
+    j += 2
+values_list = set(values_list)
+values_list = list(values_list)
+print(values_list)
