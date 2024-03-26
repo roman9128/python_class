5e71d691c8317bdad2f012a46bf306a4066a75aa
roman <roma-t@bk.ru>
2024-03-27 01:20:31 +0300

    lesson8_2

--- lesson_8/8_1.py
+++ lesson_8/8_1.py
@@ -6,9 +6,8 @@
 # (Например имя или фамилию человека)
 # 4. Использование функций. Ваша программа не должна быть линейной
 
-from easygui import *
+# from easygui import *
 import json
-from pprint import pprint 
 from os import system, name
 def clear():
 	if name == 'nt':
@@ -24,51 +23,125 @@ def load_contacts():
         with open("contacts.txt", "r", encoding = "utf-8") as contact:
             contacts = json.load(contact)
     except:
-        contacts = { 
-            "Name1": {"phones": [1234, 2341], "birthday": "01.01.2000", "email": "name1@email.internet"},
-            "Name2": {"phones": [7890], "birthday": "02.02.2002"}
+        contacts = [
+        { 
+        "name": "Пётр",
+        "surname": "Романов",
+        "phone number": "123456789",
+        "additional phone number": "23402340",
+        "birthday": "01.01.2000",
+        "email": "name1@email.internet"
+        },
+        {
+        "name": "Славик",
+        "surname": "",
+        "phone number": "78904563",
+        "additional phone number": "",
+        "birthday": "02.02.2002",
+        "email": "name1@email.internet"
+        },
+        {
+        "name": "Юрий",
+        "surname": "Иванов",
+        "phone number": "45632107",
+        "additional phone number": "3695214",
+        "birthday": "03.03.2003",
+        "email": ""
         }
+        ]
     return contacts
 
 def show(contacts):
     clear()
-    pprint(contacts)
+    for i in contacts:
+        print("___________")
+        for k, v in i.items():
+            print("{0}: {1}".format(k, v))
 
-def change(contacts):
-    item_to_change = input("Что меняем: ")
-    new_item = input("На что меняем: ")
+def find_by_name(contacts):
+    name = input("Кого ищем? ")
+    flag = False
+    for i in contacts:
+        if name.casefold() in i["name"].casefold():
+            flag = True
+            print("___________")
+            for k, v in i.items():
+                print("{0}: {1}".format(k, v))
+    if flag == False:
+        print("Ничего не нашлось")
 
+def find_by_number(contacts):
+    number = input("Введите номер телефона: ")
+    flag = False
+    for i in contacts:
+        if number in i["phone number"] or number in i["additional phone number"]:
+            flag = True
+            print("___________")
+            for k, v in i.items():
+                print("{0}: {1}".format(k, v))
+    if flag == False:
+        print("Ничего не нашлось")
+
+def find_anything(contacts):
+    name = input("Что ищем? ")
+    flag = False
+    for i in contacts:
+        if name in i.values():
+            flag = True
+            print("___________")
+            for k, v in i.items():
+                print("{0}: {1}".format(k, v))
+    if flag == False:
+        print("Ничего не нашлось")
+
+def remove(contacts):
+    print("Введите порядковый номер контакта, который хотите удалить")
+    for i in range(len(contacts)):
+        print(f"{i + 1} - {contacts[i]["name"]} {contacts[i]["surname"]}")
+    print("Чтобы выйти в меню, введите 0")
+    while True:
+        try:
+            choice = int(input())
+            if choice == 0:
+                break
+            elif choice > 0 and choice <= len(contacts):
+                print(f"Контакт {contacts[choice - 1]["name"]} {contacts[choice - 1]["surname"]} удалён")
+                del contacts[choice - 1]
+                break
+            else:
+                print("Что-то не то ввели, попробуйте ещё раз")   
+        except:
+            print("Что-то не то ввели, попробуйте ещё раз")
     return contacts
 
-def find(contacts):
-    name = input("Кого ищем? ")
-    try:
-        print(contacts[name])
-    except:
-        print("Здесь такого нет")
 
 contacts = load_contacts()
 
 while True:
-    user_choice = input("Ваше действие:\n\
-                            1 - Посмотреть справочник\n\
-                            2 - Найти контакт\n\
-                            3 - Добавить контакт\n\
-                            4 - Изменить контакт\n\
-                            5 - Удалить контакт\n\
-                            Чтобы выйти, нажмите любую иную клавишу\n")
+    user_choice = input("\
+            Ваше действие:\n\
+            1 - Посмотреть справочник\n\
+            2 - Найти человека по имени\n\
+            3 - Найти человека по номеру телефона\n\
+            4 - Добавить контакт\n\
+            5 - Изменить контакт\n\
+            6 - Удалить контакт\n\
+            Чтобы выйти, введите любой иной символ\n")
     if user_choice == "1":
         show(contacts)
     elif user_choice == "2":
         clear()
-        find(contacts)
+        find_by_name(contacts)
     elif user_choice == "3":
+        clear()
+        find_by_number(contacts)
+    elif user_choice == "4":
         add(contacts)
         save()
-    elif user_choice == "4":
+    elif user_choice == "5":
         change(contacts)
         save()
-    elif user_choice == "5":
+    elif user_choice == "6":
         remove(contacts)
         save()
     else:
